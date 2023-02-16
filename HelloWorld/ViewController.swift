7a011b7c410ddc8b65db0553e3bbff157e338ce6
Vaverka123 <maslova.vera.2019@yandex.by>
2023-02-16 13:30:36 +0400

    text logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("This is a test.")
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func makeButtonAction() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle ("Hide text", for: .normal)
+        } else {
+            toggleButton.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
 }
 
